921d45ff64ea587716902927ee78d92376b62491
Arshad\Arshay <arshadalisoomro7@gmail.com>
2020-12-27 11:35:52 +0500

    Package organization.

--- app/src/main/java/pk/team/inlab/app/todo/ui/MainActivity.kt
+++ app/src/main/java/pk/team/inlab/app/todo/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package pk.team.inlab.app.todo
+package pk.team.inlab.app.todo.ui
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import pk.team.inlab.app.todo.R
 
 class MainActivity : AppCompatActivity() {
 
