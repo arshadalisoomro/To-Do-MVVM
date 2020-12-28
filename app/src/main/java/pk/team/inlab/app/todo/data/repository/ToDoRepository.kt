899c249a0ed80176ec631df99520b4d276dc7b27
Arshad\Arshay <arshadalisoomro7@gmail.com>
2020-12-28 17:40:21 +0500

    Creating ToDoViewModel completed

--- app/src/main/java/pk/team/inlab/app/todo/data/repository/ToDoRepository.kt
+++ app/src/main/java/pk/team/inlab/app/todo/data/repository/ToDoRepository.kt
@@ -7,7 +7,7 @@ import pk.team.inlab.app.todo.data.model.ToDoData
 
 class ToDoRepository(private val toDoDao: ToDoDao) {
 
-    val getAll: Flow<List<ToDoData>> = toDoDao.getAll()
+    val getAllFlow: Flow<List<ToDoData>> = toDoDao.getAll()
 
     suspend fun insert(toDoData: ToDoData){
         toDoDao.insert(toDoData)
