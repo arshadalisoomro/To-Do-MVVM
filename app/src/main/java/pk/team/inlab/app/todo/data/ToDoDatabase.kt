0c4cddaee510970a59057d9346fc6fe0daee3f68
Arshad\Arshay <arshadalisoomro7@gmail.com>
2020-12-28 16:55:01 +0500

    Creating ToDoDatabase using Room completed

---
+++ app/src/main/java/pk/team/inlab/app/todo/data/ToDoDatabase.kt
@@ -0,0 +1,43 @@
+package pk.team.inlab.app.todo.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import pk.team.inlab.app.todo.data.dao.ToDoDao
+import pk.team.inlab.app.todo.data.model.ToDoData
+import pk.team.inlab.app.todo.util.Util
+import pk.team.inlab.app.todo.util.Util.Companion.TO_DO_DATA_NAME
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase(): RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getDatabase(context: Context): ToDoDatabase{
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ToDoDatabase::class.java,
+                        TO_DO_DATA_NAME
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+
+        }
+
+    }
+
+}
--- app/src/main/java/pk/team/inlab/app/todo/util/Util.kt
+++ app/src/main/java/pk/team/inlab/app/todo/util/Util.kt
@@ -2,6 +2,7 @@ package pk.team.inlab.app.todo.util
 
 class Util {
     companion object{
+        const val TO_DO_DATA_NAME = "to_do_db"
         const val TO_DO_TABLE_NAME = "to_do"
     }
 }
